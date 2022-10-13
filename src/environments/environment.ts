88bfea753d5c16e2b4edde923c34e51db989f60e
doanduyhien203 <43412332+doanduyhien203@users.noreply.github.com>
2022-10-13 14:00:33 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "xxxxxxxx-xxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxx",
-    storageBucket: "xxxxxxxx",
-    messagingSenderId: "xxxxxx",
-    appId: "xxxxx",
-    measurementId: "xxxxxxxxxxxxxxxx"
+   apiKey: "AIzaSyCUIwimGbxv7lPUBoSrYxl7ljb3U-z_HQ8",
+  authDomain: "angular-auth-91c1e.firebaseapp.com",
+  databaseURL: "https://angular-auth-91c1e-default-rtdb.firebaseio.com",
+  projectId: "angular-auth-91c1e",
+  storageBucket: "angular-auth-91c1e.appspot.com",
+  messagingSenderId: "64028441600",
+  appId: "1:64028441600:web:29d6ef215dd56c37206ce5",
+  measurementId: "G-9W4MD1H3P0"
   }
 };
